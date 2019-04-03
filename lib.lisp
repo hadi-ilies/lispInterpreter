4b1d158e22fafd91ae5709878d957e4bed7b55de
Gabriel Hamel <gabriel.hamel@epitech.eu>
2019-04-03 19:08:22 +0200

    Lib: Added or not and

--- lib.lisp
+++ lib.lisp
@@ -12,3 +12,18 @@
     lambda (x y)
         (if (> x y) #f #t)
 ))
+
+(define not (
+    lambda (x)
+        (if (eq? x #t) #f #t)
+))
+
+(define or (
+    lambda (x y)
+        (if (eq? x #t) #t (if (eq? y #t) #t #f))
+))
+
+(define and (
+    lambda (x y)
+        (if (eq? x #t) (if (eq? y #t) #t #f) #f)
+))
