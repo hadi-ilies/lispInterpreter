4acb62ac89b22d7d047a97572e922bc348da5ffe
Gabriel Hamel <gabriel.hamel@epitech.eu>
2019-04-04 00:11:32 +0200

    removed if

--- lib.lisp
+++ lib.lisp
@@ -1,31 +1,31 @@
 (define > (
     lambda (x y)
-        (if (eq? x y) #f (if (< x y) #f #t))
+        (cond ((eq? x y) #f) ((< x y) #f) (#t #t))
 ))
 
 (define >= (
     lambda (x y)
-        (if (< x y) #f #t)
+        (cond ((< x y) #f) (#t #t))
 ))
 
 (define <= (
     lambda (x y)
-        (if (> x y) #f #t)
+        (cond ((> x y) #f) (#t #t))
 ))
 
 (define not (
     lambda (x)
-        (if (eq? x #t) #f #t)
+        (cond ((eq? x #t) #f) (#t #t))
 ))
 
 (define or (
     lambda (x y)
-        (if (eq? x #t) #t (if (eq? y #t) #t #f))
+        (cond ((eq? x #t) #t) ((eq? y #t) #t) (#t #f))
 ))
 
 (define and (
     lambda (x y)
-        (if (eq? x #t) (if (eq? y #t) #t #f) #f)
+        (cond ((eq? x #t) (cond ((eq? y #t) #t) (#t #f))) (#t #f))
 ))
 
 (define cadr (
@@ -50,15 +50,15 @@
 
 (define null? (
     lambda (x)
-        (if (eq? x '()) #t #f)
+        (cond ((eq? x '()) #t) (#t #f))
 ))
 
 (define length (
     lambda (x)
-        (if (null? x) 0 (+ 1 (length (cdr x))))
+        (cond ((null? x) 0) (#t (+ 1 (length (cdr x)))))
 ))
 
 (define reverse (
     lambda (x)
-        (if (<= (length x) 1) x (cons (reverse (cdr x)) (car x)))
+        (cond ((<= (length x) 1) x) (#t (cons (reverse (cdr x)) (car x))))
 ))
